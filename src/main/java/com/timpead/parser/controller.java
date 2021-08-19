30eb0becca093aa3ecbe99a8e056d389950554ab
ikacar <zaekonomiku@gmail.com>
2021-08-19 23:01:05 +0200

    kraj

--- src/main/java/com/timpead/parser/controller.java
+++ src/main/java/com/timpead/parser/controller.java
@@ -16,9 +16,9 @@ import java.util.List;
 
 @RestController
 public class controller {
-    final String[] CSV_HEADER = { "ID", "ime", "Tax Status"};
     @PostMapping("parse")
     public String parse(@RequestParam("file") MultipartFile file) {
+        int number = 13800;
         if (file.isEmpty()) {
             return "prazan fajl";
         } else {
@@ -29,20 +29,110 @@ public class controller {
 
                 // read one record at a time
                 String[] record;
-                CSVWriter writer = new CSVWriter(new FileWriter("./object-list-sample.csv"));
+                CSVWriter writer = new CSVWriter(new FileWriter("./object-list-sample.csv"),CSVWriter.DEFAULT_SEPARATOR,CSVWriter.NO_QUOTE_CHARACTER,CSVWriter.NO_ESCAPE_CHARACTER);
 
                 while ((record = csvReader.readNext()) != null) {
-                    if(record[1].toString().equals("sample"))
-                    {
-                        record[1]="variable";
-                        record[25]="";
-                        writer.writeNext(record);
+                    String[] record2 = new String[47];
 
+                    for(int i = 0; i<42;i++){
+                        if(i==1){
+                            record2[1]="variable";
+                        }
+                            record2[i]=record[i];
                     }
-                    System.out.println("ID: " + record[0]);
-                    System.out.println("Name: " + record[1]);
-                    System.out.println("Email: " + record[2]);
-                    System.out.println("Country: " + record[3]);
+
+                        if(record[3].contains("ženska majica")){
+                            record2[3]="\""+record[3]+"\"";
+                            record2[26]="\""+record[26]+"\"";
+                            record2[27]="\""+record[27]+"\"";
+                            record2[43]="Veličina";
+                            record2[44]="\"XS, S, M, L, XL\"";
+                            record2[45]="1";
+                            record2[46]="1";
+                            if(record[29].contains(",")){
+                                record2[29]="\""+record[29]+"\"";
+                            }
+                            writer.writeNext(record2);
+
+                            String[] record3 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XS\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","1","","","","","Veličina","XS","","1"};
+                            String[] record4 = {String.valueOf(number++),"variation","","\""+record[3]+ " - S\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","2","","","","","Veličina","S","","1"};
+                            String[] record5 = {String.valueOf(number++),"variation","","\""+record[3]+ " - M\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","3","","","","","Veličina","M","","1"};
+                            String[] record6 = {String.valueOf(number++),"variation","","\""+record[3]+ " - L\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","4","","","","","Veličina","L","","1"};
+                            String[] record7 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XL\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","5","","","","","Veličina","XL","","1"};
+                            writer.writeNext(record3);
+                            writer.writeNext(record4);
+                            writer.writeNext(record5);
+                            writer.writeNext(record6);
+                            writer.writeNext(record7);
+
+
+                        }
+                        else if(record[3].contains("duks")){
+                            record2[43]="Veličina";
+                            record2[44]="\"S, M, L, XL, XXL, XXXL\"";
+                            record2[45]="1";
+                            record2[46]="1";
+                            record2[3]="\""+record[3]+"\"";
+                            record2[26]="\""+record[26]+"\"";
+                            record2[27]="\""+record[27]+"\"";
+                            if(record[29].contains(",")){
+                                record2[29]="\""+record[29]+"\"";
+                            }
+                            writer.writeNext(record2);
+
+                            String[] record3 = {String.valueOf(number++),"variation","","\""+record[3]+ " - S\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","1","","","","","Veličina","S","","1"};
+                            String[] record4 = {String.valueOf(number++),"variation","","\""+record[3]+ " - M\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","2","","","","","Veličina","M","","1"};
+                            String[] record5 = {String.valueOf(number++),"variation","","\""+record[3]+ " - L\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","3","","","","","Veličina","L","","1"};
+                            String[] record6 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XL\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","4","","","","","Veličina","XL","","1"};
+                            String[] record7 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XXL\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","5","","","","","Veličina","XXL","","1"};
+                            String[] record8 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XXXL\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","6","","","","","Veličina","XXXL","","1"};
+                            writer.writeNext(record3);
+                            writer.writeNext(record4);
+                            writer.writeNext(record5);
+                            writer.writeNext(record6);
+                            writer.writeNext(record7);
+                            writer.writeNext(record8);
+                        }
+                        else{
+                            record2[43]="Veličina";
+                            record2[44]="\"S, M, L, XL, XXL, XXXL\"";
+                            record2[45]="1";
+                            record2[46]="1";
+                            record2[3]="\""+record[3]+"\"";
+                            record2[26]="\""+record[26]+"\"";
+                            record2[27]="\""+record[27]+"\"";
+                            if(record[29].contains(",")){
+                                record2[29]="\""+record[29]+"\"";
+                            }
+                            writer.writeNext(record2);
+                            String[] record3 = {String.valueOf(number++),"variation","","\""+record[3]+ " - S\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","1","","","","","Veličina","S","","1"};
+                            String[] record4 = {String.valueOf(number++),"variation","","\""+record[3]+ " - M\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","2","","","","","Veličina","M","","1"};
+                            String[] record5 = {String.valueOf(number++),"variation","","\""+record[3]+ " - L\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","3","","","","","Veličina","L","","1"};
+                            String[] record6 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XL\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","4","","","","","Veličina","XL","","1"};
+                            String[] record7 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XXL\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","5","","","","","Veličina","XXL","","1"};
+                            String[] record8 = {String.valueOf(number++),"variation","","\""+record[3]+ " - XXXL\"","1","0","visible","","","","","taxable","parent","1","","","0","0","","","","","0","","",record[25],"","","","","","","id:"+record[0],"","","","","","6","","","","","Veličina","XXXL","","1"};
+                            writer.writeNext(record3);
+                            writer.writeNext(record4);
+                            writer.writeNext(record5);
+                            writer.writeNext(record6);
+                            writer.writeNext(record7);
+                            writer.writeNext(record8);
+                        }
+
+
+
+
+//                    if(record[3].contains("ženska majica"))
+//                    {
+//
+//                        record[25]="";
+//                        writer.writeNext(record);
+//
+//                    }
+//                    System.out.println("ID: " + record[0]);
+//                    System.out.println("Name: " + record[1]);
+//                    System.out.println("Email: " + record[2]);
+//                    System.out.println("Country: " + record[3]);
                 }
 
                 // close readersx
@@ -76,6 +166,7 @@ public class controller {
 //                        .build();
 //                beanToCsv.write(rowOutList);
             } catch (Exception ex) {
+                ex.printStackTrace();
                 return "greska";
             }
             return "ok";
